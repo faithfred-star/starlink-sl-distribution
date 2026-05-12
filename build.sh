5dad71db4b7976cf9b005d74eaa18c260582c13b
faithfred-star <faithfred721@gmail.com>
2026-05-12 22:38:57 +0300

    Make build.sh executable

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-
+# Exit on error
 set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
+# Collect static files for WhiteNoise
+python manage.py collectstatic --no-input
+
+# Apply database migrations
 python manage.py migrate
